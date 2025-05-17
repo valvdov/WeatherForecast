570b4bd4cf592a57bea2fd7ee28268c15c3b87b8
Valerii <valvdov@gmail.com>
2025-05-17 18:14:16 +0100

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Valera Vdovin on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
